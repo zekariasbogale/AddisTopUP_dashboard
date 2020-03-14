09a287b8664de1883c87350515b61a543a1aa41f
Zekarias Bogale <zekarias.bogale@gmail.com>
2020-03-14 20:01:32 +0300

    Add init files via upload
    Add index.html, script.js & test.csv

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4ugIzhOkLQw0rXPXVzmnzPBPbc73sMNI",
+  authDomain: "addis-topup.firebaseapp.com",
+  databaseURL: "https://addis-topup.firebaseio.com",
+  projectId: "addis-topup",
+  storageBucket: "addis-topup.appspot.com",
+  messagingSenderId: "342067385558",
+  appId: "1:342067385558:web:b37784e5f3ec75b25f2577",
+  measurementId: "G-K9QJMCPR10"
+};
+firebase.initializeApp(firebaseConfig);
