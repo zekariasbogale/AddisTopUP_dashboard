4c9328cf078c4140b85b12b8c796136ac8737199
Zekarias Bogale <zekarias.bogale@gmail.com>
2020-04-01 15:07:21 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,54 @@
+<!-- Start web template header maintained by Zekarias Bogale -->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Addis TopUP</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://addis.brootech.com/index.html">EVD Dashboard</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="https://addis.brootech.com/agents.html">Manage Agents</a></li>
+                <li><a href="https://addis.brootech.com/evs.html">Manage EVs</a></li>
+                  <ul class="nav navbar-nav">
+                      <li><a href="https://addis.brootech.com/upload_evs.html">Upload EVs</a></li>
+                      <li><a href="https://addis.brootech.com/distribute_evs.html">Distribute EVs</a></li>
+                  </ul>
+                <li><a href="#">Transactions</a></li>
+                <li><a href="#">Activity Log</a></li>
+                <li><a href="#">User: root</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
+
+
+<div class="container">
+
+
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading"></div>
+
+                <div class="panel-body">
+
+<!-- End web template header -->
