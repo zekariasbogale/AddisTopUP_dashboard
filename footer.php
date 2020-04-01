977a66e463cbc723a9bdfd75f06d22fe467ac320
Zekarias Bogale <zekarias.bogale@gmail.com>
2020-04-01 15:03:12 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- Start web template footer maintained by Zekarias Bogale-->
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="text-center panel-footer">
+Copyright © 2020 <a href="https://www.brootech.com">Brootech IT Solution</a>. All rights reserved.
+</footer>
+
+</body>
+</html>
+
+<!-- End web template footer -->
