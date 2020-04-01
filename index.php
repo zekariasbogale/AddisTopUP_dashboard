d748f2cdf854fd622451621813738d4875168ae3
Zekarias Bogale <zekarias.bogale@gmail.com>
2020-04-01 15:29:32 +0300

    Update and rename index.html to index.php
    Use require statement and update code and comment to php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php require './header.php'?>
+
+<?php
+
+/**
+  * EVD Dashboard Landing Page Place Holder 
+ */
+
+echo "EVD Dashboard Landing Page ... Click on either /"Upload EVs/" or /"Distribute EVs/""
+
+?>
+
+<?php require './footer.php'?>
