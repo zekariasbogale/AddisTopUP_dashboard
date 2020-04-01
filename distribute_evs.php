a2bf05a20d793790984c45341dc5903affb45b5e
Zekarias Bogale <zekarias.bogale@gmail.com>
2020-04-01 16:55:14 +0300

    Create distribute_evs.php

---
+++ distribute_evs.php
@@ -0,0 +1,12 @@
+<?php require './header.php'?>
+
+<?php
+
+// Distribute Electronic Vouchers (EVs) to agents from the master EV MySQL database to Firebase distribution batch collection
+// Bire add your code here
+
+echo "Distribute Electronic Vouchers (EVs)"
+
+?>
+
+<?php require './footer.php'?>
